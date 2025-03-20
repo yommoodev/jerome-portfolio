e0364514bcd869b47e608dc347e4c5248b231568
yommoodev <jvillanueva2698@gmail.com>
2025-03-20 14:08:28 +0800

    change the footer date

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="py-[64px]" data-aos="fade-up" data-aos-duration="1000">
-        <p className="text-center font-semibold text-white">© 2023 John Doe. All rights reserved.</p>
+        <p className="text-center font-semibold text-white">© 2025 Jerome Canolang. All rights reserved.</p>
     </footer>
   )
 }
